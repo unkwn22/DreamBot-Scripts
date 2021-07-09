cdb206892bf298af67084df25532b399bb563222
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-09 18:33:37 +0900

    Comments added

--- src/GnomeStrongholdAgilityCourse.java
+++ src/GnomeStrongholdAgilityCourse.java
@@ -35,6 +35,7 @@ public class GnomeStrongholdAgilityCourse extends AbstractScript {
     Tile net2Tile = new Tile(2485, 3426);
     Tile pipeTile = new Tile(2484, 3431);
 
+    //getGameObjects has been deprecated -> GameObject.closest()...
     GameObject log;
     GameObject net1;
     GameObject tree1;
@@ -43,48 +44,15 @@ public class GnomeStrongholdAgilityCourse extends AbstractScript {
     GameObject net2;
     GameObject pipe;
 
-    @Override
-    public int onLoop() {
-
-        if(getState().equals(State.LOG)){
-            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Log balance") && c.getTile().equals(logTile));
-            log.interact("Walk-across");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endLogTile), 10000);
-        }else if(getState().equals(State.NET1)){
-            net1 = GameObjects.closest(c -> c != null && c.getName().contentEquals("Obstacle net") && c.getTile().equals(net1Tile));
-            net1.interact("Climb-over");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endNet1Tile), 6000);
-        }else if(getState().equals(State.TREE1)){
-            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Tree branch") && c.getTile().equals(tree1Tile));
-            log.interact("Climb");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endTree1Tile), 6000);
-        }else if(getState().equals(State.ROPE)){
-            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Balancing rope") && c.getTile().equals(ropeTile));
-            log.interact("Walk-on");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endRopeTile), 6000);
-        }else if(getState().equals(State.TREE2)){
-            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Tree branch") && c.getTile().equals(tree2Tile));
-            log.interact("Climb-down");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endTree2Tile), 6000);
-        }else if(getState().equals(State.NET2)){
-            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Obstacle net") && c.getTile().equals(net2Tile));
-            log.interact("Climb-over");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endNet2Tile), 6000);
-        }else if(getState().equals(State.PIPE)){
-            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Obstacle pipe") && c.getTile().equals(pipeTile));
-            log.interact("Squeeze-through");
-            sleepUntil(() -> getLocalPlayer().getTile().equals(endPipeTile), 10000);
-        }
-        return 500;
-    }
-
-    //
     private enum State{
         LOG, NET1, TREE1, ROPE, TREE2, NET2, PIPE
     }
 
+    //indicating the player's situation
     private State getState(){
+        //start is the starting radius we indicated
         if(start.contains(getLocalPlayer()) || getLocalPlayer().getTile().equals(endPipeTile)){
+            //then the next task the player should do is LOG enum
             state = State.LOG;
         }else if(getLocalPlayer().getTile().equals(endLogTile)){
             state = State.NET1;
@@ -100,6 +68,45 @@ public class GnomeStrongholdAgilityCourse extends AbstractScript {
             state = State.PIPE;
         }
         return state;
+    }
+
+    @Override
+    public int onLoop() {
+        //the loop is constantly getting the state of  the player every tick
+        if(getState().equals(State.LOG)){
+            //indicating the log object name and its position
+            log = GameObjects.closest(c -> c != null && c.getName().contentEquals("Log balance") && c.getTile().equals(logTile));
+            //interact transaction
+            log.interact("Walk-across");
+                                                                        //giving the bot a flexible time inorder to proceed to the next task
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endLogTile), 10000);
+        }else if(getState().equals(State.NET1)){
+            net1 = GameObjects.closest(c -> c != null && c.getName().contentEquals("Obstacle net") && c.getTile().equals(net1Tile));
+            net1.interact("Climb-over");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endNet1Tile), 6000);
+        }else if(getState().equals(State.TREE1)){
+            tree1 = GameObjects.closest(c -> c != null && c.getName().contentEquals("Tree branch") && c.getTile().equals(tree1Tile));
+            log.interact("Climb");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endTree1Tile), 6000);
+        }else if(getState().equals(State.ROPE)){
+            rope = GameObjects.closest(c -> c != null && c.getName().contentEquals("Balancing rope") && c.getTile().equals(ropeTile));
+            log.interact("Walk-on");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endRopeTile), 6000);
+        }else if(getState().equals(State.TREE2)){
+            tree2 = GameObjects.closest(c -> c != null && c.getName().contentEquals("Tree branch") && c.getTile().equals(tree2Tile));
+            log.interact("Climb-down");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endTree2Tile), 6000);
+        }else if(getState().equals(State.NET2)){
+            net2 = GameObjects.closest(c -> c != null && c.getName().contentEquals("Obstacle net") && c.getTile().equals(net2Tile));
+            log.interact("Climb-over");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endNet2Tile), 6000);
+        }else if(getState().equals(State.PIPE)){
+            pipe = GameObjects.closest(c -> c != null && c.getName().contentEquals("Obstacle pipe") && c.getTile().equals(pipeTile));
+            log.interact("Squeeze-through");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(endPipeTile), 10000);
+        }
+
+        return 0;
     }
 
 }
