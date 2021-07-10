cb5a4cf0917b23d481033441ebdb8ea5bfd15034
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-10 09:33:30 +0900

    fix intensive clicking and sleepUntil condition

--- src/FishingGuild.java
+++ src/FishingGuild.java
@@ -13,6 +13,8 @@ import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.wrappers.interactive.NPC;
 import org.dreambot.api.wrappers.items.GroundItem;
 
+import java.util.Random;
+
 @ScriptManifest(name = "FishingGuild", description = "Fourth Script", author = "lsjc12911",
         version = 1.0, category = Category.FISHING, image = "")
 
@@ -20,45 +22,60 @@ public class FishingGuild extends AbstractScript{
 
     State state;
 
+    Random randomNum = new Random();
+
     Tile fishingTile = new Tile(2599, 3422);
     Area fishingArea = new Area(2595, 3419, 2605, 3426);
+    Tile bankTile = new Tile(2587, 3419);
     Area bankArea = new Area(2586, 3418, 2588, 3421);
 
     NPC fishingSpot;
     GameObject bank;
 
     private enum State{
-        FISHING, BANKING, MOVE
+        FISH, FISHING, BANKING, MOVE2FISH, MOVE2BANK
     }
 
     private State getState(){
 
         if(fishingArea.contains(getLocalPlayer()) && getLocalPlayer().getAnimation() == -1 && !Inventory.isFull()){
-            state = State.FISHING;
-        }else if(fishingArea.contains(getLocalPlayer()) && getLocalPlayer().getAnimation() == -1 && Inventory.isFull()){
+            log("fish");
+            state = State.FISH;
+            sleep(1000, 1050);
+        } else if(fishingArea.contains(getLocalPlayer()) && getLocalPlayer().getAnimation() != -619 && Inventory.isFull()){
             log("banking");
+            state = State.MOVE2BANK;
+        } else if(bankArea.contains(getLocalPlayer()) && Inventory.isFull()){
             state = State.BANKING;
         } else if(bankArea.contains(getLocalPlayer()) && !Inventory.isFull()){
-            state = State.MOVE;
+            state = State.MOVE2FISH;
+        } else {
+            state = State.FISHING;
         }
         return state;
     }
 
     @Override
     public int onLoop() {
-        if(getState().equals(State.FISHING)){
+        if(getState().equals(State.FISH)){
             fishingSpot = NPCs.closest(f -> f != null && f.getName().contentEquals("Fishing spot") && fishingArea.contains(f));
             fishingSpot.interact("Cage");
-            sleepUntil(Inventory::isFull, 100000);
-        }else if(getState().equals(State.BANKING)){
+            sleepUntil(() -> getLocalPlayer().getAnimation() == -1, 100000);
+        } else if(getState().equals(State.BANKING)){
             if(Bank.openClosest()){
                 Bank.deposit("Raw lobster", 27);
+                if(Bank.count("Raw lobster") == 600){
+                    stop();
+                }
             }else {
                 sleepUntil(Bank::isOpen, 3000);
             }
-        }else if(getState().equals(State.MOVE)){
+        } else if(getState().equals(State.MOVE2FISH)){
             Walking.walk(fishingTile);
             sleepUntil(() -> getLocalPlayer().getTile().equals(fishingTile), 1000);
+        } else if(getState().equals(State.MOVE2BANK)){
+            Walking.walk(bankTile);
+            sleepUntil(() -> getLocalPlayer().getTile().equals(bankTile), 1000);
         }
         return 0;
     }
