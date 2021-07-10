f407a21926bbc0dc37a9aa65cdf098969b2b960e
27조 <79817453+unkwn22@users.noreply.github.com>
2021-07-11 06:55:30 +0900

    fix: healing priority, changed state for not pickpocketing

--- src/Thieving.java
+++ src/Thieving.java
@@ -1,5 +1,11 @@
 import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.api.methods.interactive.NPCs;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.methods.tabs.Tab;
+import org.dreambot.api.methods.tabs.Tabs;
+import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
@@ -13,22 +19,37 @@ public class Thieving extends AbstractScript {
     State state;
 
     String food = "Lobster";
+    String targetNpc = "Warrior woman";
+
+    Area pickingArea = new Area(2624, 3307, 2643, 3288);
+    Tile pickTile = new Tile(2631, 3295);
+    Area bankArea = new Area(2649, 3286, 2654, 3281);
+    Tile bankTile = new Tile(2649, 3284);
 
     private enum State{
-        THIEVING, OPEN_ALL, STUNNED, HEAL
+        THIEVING, OPEN_ALL, STUNNED, HEAL, MOVE2BANK, RESTOCK, MOVE2PICK
     }
 
     //indicating the player's situation
     private State getState(){
         //start is the starting radius we indicated
         log(getLocalPlayer().getHealthPercent());
+
         if(Inventory.count("Coin pouch") == 28){
             state = State.OPEN_ALL;
-        } else if(getLocalPlayer().getAnimation() == -1 && Inventory.count("Coin pouch") < 28){
+        } else if(!getLocalPlayer().isInteractedWith() && Inventory.count("Coin pouch") < 28 && pickingArea.contains(getLocalPlayer())){
            state = State.THIEVING;
         } else if(getLocalPlayer().getAnimation() == 424){
             state = State.STUNNED;
-        } else if(getLocalPlayer().getHealthPercent() <= 32){
+        } else if(!Inventory.contains(food)){
+            state = State.MOVE2BANK;
+        } else if(bankArea.contains(getLocalPlayer()) && !Inventory.contains(food)){
+            state = State.RESTOCK;
+        } else if(bankArea.contains(getLocalPlayer()) && Inventory.contains(food)){
+            state = State.MOVE2PICK;
+        }
+
+        if(getLocalPlayer().getHealthPercent() <= 32) {
             state = State.HEAL;
         }
         return state;
@@ -38,8 +59,9 @@ public class Thieving extends AbstractScript {
     public int onLoop() {
         //the loop is constantly getting the state of  the player every tick
         if(getState().equals(State.THIEVING)){
-            NPC man = NPCs.closest("MAN");
-            man.interact("Pickpocket");
+            NPC npc = NPCs.closest(targetNpc);
+            npc.interact("Pickpocket");
+            log("pickpocketing");
             sleep(500, 1500);
         }else if(getState().equals(State.OPEN_ALL)){
             Inventory.interact("Coin pouch", "Open-all");
@@ -48,7 +70,25 @@ public class Thieving extends AbstractScript {
             log("Stunned!");
             sleep(3500);
         }else if(getState().equals(State.HEAL)){
+            log("Eating!");
             Inventory.interact(food, "Eat");
+            sleep(1000);
+        }else if(getState().equals(State.MOVE2BANK)){
+            Walking.walk(bankTile);
+            log("Moving to bank");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(bankTile), 6000);
+        }else if(getState().equals(State.RESTOCK)){
+            log("BANKING");
+            if(Bank.openClosest()){
+                Bank.withdraw("Raw lobster", 25);
+                Bank.close();
+            }else {
+                sleepUntil(Bank::isOpen, 4000);
+            }
+        }else if(getState().equals(State.MOVE2PICK)){
+            Walking.walk(pickTile);
+            log("Moving back to pick location");
+            sleepUntil(() -> getLocalPlayer().getTile().equals(pickTile), 6000);
         }
 
         return 0;
